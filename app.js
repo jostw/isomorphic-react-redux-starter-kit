6bd63a0268adfe8747fb0756730e3d67a637cadf
jos <mail@jos.tw>
2016-05-13 16:31:22 +0800

    Add express and babel

---
+++ app.js
@@ -0,0 +1,22 @@
+/*
+ * isomorphic-react-redux-starter-kit
+ *
+ * https://github.com/jostw/isomorphic-react-redux-starter-kit
+ *
+ * Copyright (c) 2016 jos
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+import express from 'express';
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
